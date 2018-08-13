989714839efe780b3e0abfa6b0e4759aa1ac43f0
Patrick Cheng <pat.rick.cheng@hotmail.com>
2018-08-13 06:02:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // add solution here
 function theBeatlesPlay(musicians, instruments){
   var answer = []
-  for(var x=0;x< musicians.length();x++){
+  for(var x=0;x< musicians.length;x++){
     answer.push('${musicians[x]} plays ${instruments[x]}')
   }
   return answer
